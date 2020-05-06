107896f17369ff2eb051fb9f3acb5f8460a28419
Li-Hsuan <lihsuanlung@gmail.com>
2020-05-06 15:09:45 -0400

    Step 2

--- rcv.rb
+++ rcv.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
 class RCV
-  def count(votes:, candidates:); end
+  def count(votes:, candidates:)
+    if candidates.empty?
+      nil
+    else
+      candidate_votes = Hash.new(0)
+      votes.each do |candidate, *rest|
+        candidate_votes[candidate] += 1
+      end
+      highest_vote_count = candidate_votes.values.max
+      winners = candidate_votes.select do |candidate, vote_count|
+        vote_count == highest_vote_count
+      end.collect(&:first)
+      winners.first
+    end
+  end
 end
--- rcv_spec.rb
+++ rcv_spec.rb
@@ -8,5 +8,37 @@ describe RCV do
       subject { RCV.new.count(votes: [], candidates: []) }
       it { is_expected.to be_nil }
     end
+
+    context 'one vote and one winner' do
+      subject do
+        RCV.new.count(
+          votes: [
+            [:liz],
+          ],
+          candidates: [
+            :liz,
+            :joe
+          ]
+        )
+      end
+      it { is_expected.to eq(:liz) }
+    end
+
+    context 'win by majority' do
+      subject do
+        RCV.new.count(
+          votes: [
+            [:liz],
+            [:joe],
+            [:joe]
+          ],
+          candidates: [
+            :liz,
+            :joe
+          ]
+        )
+      end
+      it { is_expected.to eq(:joe) }
+    end
   end
 end
